2037bf015d2f8c16f0a50211f0c630048fa804c2
Eduardo Coello <eduardongua@hotmail.com>
2025-09-08 12:30:21 -0700

    trabaje sobre el dropdown menu incluido en la barra de navegacion

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,35 +1,44 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white', 'dropdownClasses' => ''])
+@props([
+    'align' => 'right',
+    'width' => 'sm',
+    'contentClasses' => 'dropdown__box__content',
+    'dropdownClasses' => ''
+])
 
 @php
 $alignmentClasses = match ($align) {
-    'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
-    'top' => 'origin-top',
-    'none', 'false' => '',
-    default => 'ltr:origin-top-right rtl:origin-top-left end-0',
+    'left'          => 'dropdown--left',
+    'right'         => 'dropdown--right',
+    'top'           => 'dropdown--top',
+    'top-left'      => 'dropdown--top-left',
+    'top-right'     => 'dropdown--top-right',
+    'none', 'false' => 'dropdown--none',
+    default         => 'dropdown--right',
 };
 
-$width = match ($width) {
-    '48' => 'w-48',
-    '60' => 'w-60',
-    default => 'w-48',
+$widthClasses = match ($width) {
+    'sm' => 'dropdown--sm',
+    'md' => 'dropdown--md',
+    default => 'dropdown--sm',
 };
 @endphp
 
-<div class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
+<div class="dropdown" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
     <div @click="open = ! open">
         {{ $trigger }}
     </div>
 
     <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="transform opacity-0 scale-95"
-            x-transition:enter-end="transform opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="transform opacity-100 scale-100"
-            x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
-            style="display: none;"
-            @click="open = false">
+        x-transition:enter.duration.200ms
+        x-transition:enter-start.scale.95
+        x-transition:enter-end.scale.100
+        x-transition:leave.duration.75ms
+        x-transition:leave-start.scale.100
+        x-transition:leave-end.scale.95
+        class="dropdown__box box-shadow-3 {{ $widthClasses }} {{ $alignmentClasses }} {{ $dropdownClasses }}"
+        style="display: none;"
+        @click="open = false"
+    >
         <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
             {{ $content }}
         </div>
