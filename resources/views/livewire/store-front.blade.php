772c51b333f5f00bd9a4e09bbaede1de8291229e
Eduardo Coello <eduardongua@hotmail.com>
2025-09-10 14:12:25 -0700

    ya agregue todas las colecciones al store front

--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -94,24 +94,87 @@
             <article class="collection" data-type="full-price">
                 <header class="collection__header">
                     <a href="{{ route('collection', $atemporal) }}" class="clr-primary-800 no-decor uppercase ff-bold fs-700">{{ $atemporal->name }}</a>
+                    <div class="slide-buttons">
+                        <x-icon data-type="arrow" data-type="arrow" orientation="left">
+                            <x-ui.icons.arrow />
+                        </x-icon>
+                        <x-icon data-type="arrow">
+                            <x-ui.icons.arrow />
+                        </x-icon>
+                    </div>
                 </header>
     
                 <div class="collection__slider">
                     @foreach ($atemporal->products as $product)
-                        <a
-                            class="slide | no-decor"
-                            wire:navigate
-                            href="{{ route('product', $product) }}"
-                        >
-                            <div class="slide__media">
-                                <img class="slide__image" src="{{ $product->getFirstMediaUrl('images') }}" alt="{{ $product->name }}">
-                            </div>    
-                            <div class="slide__meta">
-                                <h2 class="product">{{ $product->name }}</h2>
-                                <p class="price">{{ $product->price }}</p>
-                            </div>
-                            <button class="button" data-type="cart">Agregar al carrito</button>
-                        </a>
+                        <article class="slide">
+                            <a
+                                class="slide__link | no-decor"
+                                wire:navigate
+                                href="{{ route('product', $product) }}"
+                            >
+                                <div class="slide__media">
+                                    <img class="slide__image" src="{{ $product->getFirstMediaUrl('images') }}" alt="{{ $product->name }}">
+                                </div>    
+                                <div class="slide__meta">
+                                    <h2 class="product">{{ $product->name }}</h2>
+                                    <p class="price">{{ $product->price }}</p>
+                                </div>
+                            </a>
+                            <button
+                                class="button"
+                                data-type="cart"
+                                type="button"
+                                wire:click.prevent="addToCart({{ $product->id }})"
+                                aria-label="Agregar {{ $product->name }} al carrito"
+                            >
+                                Agregar al carrito
+                            </button>
+                        </article>
+                    @endforeach
+                </div>
+            </article>
+        @endif
+
+        @if ($comodidad)
+            <article class="collection" data-type="full-price">
+                <header class="collection__header">
+                    <a href="{{ route('collection', $comodidad) }}" class="clr-primary-800 no-decor uppercase ff-bold fs-700">{{ $comodidad->name }}</a>
+                    <div class="slide-buttons">
+                        <x-icon data-type="arrow" data-type="arrow" orientation="left">
+                            <x-ui.icons.arrow />
+                        </x-icon>
+                        <x-icon data-type="arrow">
+                            <x-ui.icons.arrow />
+                        </x-icon>
+                    </div>
+                </header>
+    
+                <div class="collection__slider">
+                    @foreach ($comodidad->products as $product)
+                        <article class="slide">
+                            <a
+                                class="slide__link | no-decor"
+                                wire:navigate
+                                href="{{ route('product', $product) }}"
+                            >
+                                <div class="slide__media">
+                                    <img class="slide__image" src="{{ $product->getFirstMediaUrl('images') }}" alt="{{ $product->name }}">
+                                </div>    
+                                <div class="slide__meta">
+                                    <h2 class="product">{{ $product->name }}</h2>
+                                    <p class="price">{{ $product->price }}</p>
+                                </div>
+                            </a>
+                            <button
+                                class="button"
+                                data-type="cart"
+                                type="button"
+                                wire:click.prevent="addToCart({{ $product->id }})"
+                                aria-label="Agregar {{ $product->name }} al carrito"
+                            >
+                                Agregar al carrito
+                            </button>
+                        </article>
                     @endforeach
                 </div>
             </article>
