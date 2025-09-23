05f5e6baa8132a2f5cb21b1a7936513d059322bf
Eduardo Coello <eduardongua@hotmail.com>
2025-09-23 14:18:33 -0700

    pulida a los estilos moviles en la PDP. instale un plugin de alpine para evitar scroll en elementos con open

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,26 @@
 import './bootstrap';
 import '../../vendor/masmerise/livewire-toaster/resources/js';
+
+import focus from '@alpinejs/focus';
+
+document.addEventListener('alpine:init', () => {
+    // focus
+    Alpine.plugin(focus)
+
+    // store global
+    Alpine.store('ui', { navOpen:false, filtersOpen:false });
+                    
+    // componente reutilizable para breakpoints
+    Alpine.data('filtersShell', () => ({
+        isDesktop: false,
+        init() {
+            const mql = window.matchMedia('(min-width: 1024px)');
+            const set = e => {
+                this.isDesktop = e.matches;
+                if (this.isDesktop) $store.ui.filtersOpen = false; // cierra sheet al pasar a desktop
+            };
+            set(mql);
+            (mql.addEventListener ? mql.addEventListener('change', set) : mql.addListener(set));
+            }
+    }));
+});
